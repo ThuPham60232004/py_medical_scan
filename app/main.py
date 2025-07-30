7784acaa6c8ddc5ea4ec8c61259000d37f191167
PHẠM THỊ ANH THƯ <165041915+ThuPham60232004@users.noreply.github.com>
2025-07-30 17:33:56 +0700

    Revert to commit 12154fd

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, UploadFile, File
 from pathlib import Path
 import threading
-from app.test import test_process_pipeline
+from app.test import main
 app = FastAPI()
 
 @app.on_event("startup")
 def startup_event():
     """Tự động quét và xử lý ảnh khi server khởi động"""
-    thread = threading.Thread(target=test_process_pipeline) 
+    thread = threading.Thread(target=main) 
     thread.start()
 @app.get("/")
 async def root():
